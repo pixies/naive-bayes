ae64667347e12233487fcdf9ca76dc6743735227
H Jay Simpson <hjaysimpsoncoorp@gmail.com>
2019-04-17 12:46:52 -0300

    initial main

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+from srcbase.N_Bayer import 
+
+
+
+def main():
+	filename = 'pima-indians-diabetes.data.csv'
+	splitRatio = 0.67
+	dataset = loadCsv(filename)
+	trainingSet, testSet = splitDataset(dataset, splitRatio)
+	print('Split {0} rows into train={1} and test={2} rows').format(len(dataset), len(trainingSet), len(testSet))
+	# prepare model
+	summaries = summarizeByClass(trainingSet)
+	# test model
+	predictions = getPredictions(summaries, testSet)
+	accuracy = getAccuracy(testSet, predictions)
+	print('Accuracy: {0}%').format(accuracy)
+
+main()
